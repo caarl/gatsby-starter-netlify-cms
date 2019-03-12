3ee9854136c913b57df97f5a51606774aa51738b
Carl Simpson <carl.simpson@zeninternet.co.uk>
2019-03-12 22:52:36 +0000

    more bob

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -158,6 +158,7 @@ export const pageQuery = graphql`
 query IndexPageTemplate {
   markdownRemark(frontmatter: {templateKey: {eq: "index-page"}}) {
       frontmatter {
+        bob
         title
         image {
           childImageSharp {
