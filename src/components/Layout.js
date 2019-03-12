5820e3632af891dfa0b8154c55cf3f9e5a2dfc5b
Carl Simpson <carl.simpson@zeninternet.co.uk>
2019-03-12 22:38:09 +0000

    title test should be lan_cou

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -59,7 +59,7 @@ const TemplateWrapper = ({ children }) => (
           <meta property="og:url" content="/" />
           <meta property="og:image" content="/img/og-image.jpg" />
         </Helmet>
-        <Navbar lang_country="red" />
+        <Navbar lang_country={data.site.siteMetadata.title} />
         <div>{children}</div>
         <Footer />
       </div>
